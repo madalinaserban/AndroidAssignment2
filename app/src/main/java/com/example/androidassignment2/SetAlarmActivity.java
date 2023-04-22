1f29ea04572972c860614be329abae97d0064f0c
Madalina <madalina_serban01@yahoo.com>
2023-04-22 16:25:18 +0300

    used shared preferences to store alarm

--- app/src/main/java/com/example/androidassignment2/SetAlarmActivity.java
+++ app/src/main/java/com/example/androidassignment2/SetAlarmActivity.java
@@ -1,13 +1,17 @@
 package com.example.androidassignment2;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Locale;
@@ -26,14 +30,21 @@ public class SetAlarmActivity extends AppCompatActivity {
         saveButton = findViewById(R.id.saveButton);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
                 int hour = timePicker.getHour();
                 int minute = timePicker.getMinute();
 
-                // Save the selected time to a database or SharedPreferences
-                // ...
+                //used SharedPreferences to store alarms
 
+                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("alarm_hour", hour);
+                editor.putInt("alarm_minute", minute);
+                editor.apply();
+
+                Toast.makeText(getApplicationContext(),"New alarm set for "+hour+" "+minute,Toast.LENGTH_LONG).show();
                 finish(); // Close the activity
             }
         });
